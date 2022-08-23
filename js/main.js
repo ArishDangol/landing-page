39475db5545aaaa52e8586cfb448d080c2b4a4f8
arish dangol <arish.dangol@gmail.com>
2022-08-23 18:36:06 +0545

    feat added-  testimonials and FAQ

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 $(document).ready(function () {
 
+ /*----- Navbar Shrink -----*/
+ $(window).on("scroll",function(){
+    if($(this).scrollTop() > 70){
+        $(".navbar").addClass("navbar-shrink");
+        $(".navbar-brand").removeClass("d-lg-none");
+    }
+    else{
+        $(".navbar").removeClass("navbar-shrink");
+        $(".navbar-brand").addClass("d-lg-none");
+    }
+});
+
+
      /*----- Video Popup -----*/
     const videoSrc = $("#player-1").attr("src");
     $(".video-play-btn, .video-popup").on("click", function () {
@@ -14,4 +27,81 @@ $(document).ready(function () {
             }
         }
     });
+
+    /*----- Features Carousel -----*/
+    $('.features-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        autoplay:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:3,
+            }
+        }
+    });
+
+    /*----- Screenshots Carousel -----*/
+    $('.screenshots-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        autoplay:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:4,
+            }
+        }
+    });
+
+     /*----- Testimonials Carousel -----*/
+     $('.testimonials-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        autoplay:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:3,
+            }
+        }
+    });
+
+    /*----- Team Carousel -----*/
+    $('.team-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        autoplay:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:3,
+            }
+        }
+    })
+
 });
